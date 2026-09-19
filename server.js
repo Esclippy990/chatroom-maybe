da5189ea0766c38b79f8986decd43927aa276ff2
Esclippy990 <153932585+Esclippy990@users.noreply.github.com>
2026-09-19 11:24:30 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ const server = http.createServer((req, res) => {
   let sn = new Date();
   let a = sn.getDate();
   let b = sn.getMonth();
-  if (req.url.startsWith('/alive') {
+  if (req.url.startsWith('/alive')) {
   res.end('YES BRO IM ALIVE')
   } else if (req.url.startsWith("/file:")) {
     // CREDITS TO GEMINI FOR HELPING ME FIX THE FORWARD-SCRUBBING ISSUE.
